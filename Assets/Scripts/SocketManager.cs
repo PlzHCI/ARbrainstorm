e28548d6e4289d9086cfd8c814d6b6b6dc9b768a
Carton Liu <1435742009@qq.com>
2024-11-05 10:37:49 -0600

    change rock to stop hand posr for ideation and update visual text

--- Assets/Scripts/SocketManager.cs
+++ Assets/Scripts/SocketManager.cs
@@ -11,7 +11,7 @@ public class SocketManager : MonoBehaviour
     void Start()
     {
         //TODO: check the Uri if Valid.
-        var uri = new Uri("http://10.131.54.64:3000/");
+        var uri = new Uri("http://10.131.194.240:3000/");
         socket = new SocketIOUnity(
             uri,
             new SocketIOOptions
